e64ce2dcd6997c1c0d4cf6c8ad24dcc0362d8fa7
unknown <will-f1@hotmail.com>
2023-03-09 12:53:09 -0300

    actualizando reloj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import PonerAlarma from './components/PonerAlarma.js';
-
+import Reloj from './components/Reloj';
 function App() {
   return (
     <div className="App">
       <section className="container">
          <div id="Clock" className="container__Clock">
+          <Reloj/>
           </div>
          <div>
           <PonerAlarma/>
--- src/components/Reloj.js
+++ src/components/Reloj.js
@@ -1,34 +1,23 @@
+import { useState, useEffect } from "react";
 
-setInterval(displayTime, 1000); 
 
 
-export default function displayTime() {
-   
-    const timeNow = new Date();
-    
-    
-    let hoursOfDay = timeNow.getHours();
-    let minutes = timeNow.getMinutes();
-    let seconds = timeNow.getSeconds();
-    let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let today = weekDay[timeNow.getDay()];
-    let months = timeNow.toLocaleString("default", {
-        month: "long"
-    });
-    let year = timeNow.getFullYear();
-    let session = "AM";
-
-    if (hoursOfDay > 12) {
-        session = "PM";
-    }
-
-    hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    let time = hoursOfDay + ":" + minutes + ":" + seconds + " ";
-    
-    // return "The time is: " + time + session + " " + today + " " + months + " " + year;
-    document.getElementById('Clock').innerHTML = "The time is: " + time + session + " " + today + " " + months + " " + year;
-    
-}   
+export default function Reloj() {
+    const [dateState, setDateState] = useState(new Date());
+    useEffect(() => {
+      setInterval(() => setDateState(new Date()), 1000);
+    }, []);
+  
+    return (
+      <div>
+        <p>
+          {dateState.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: false,
+          })}
+        </p>
+      </div>
+    );
+  }
